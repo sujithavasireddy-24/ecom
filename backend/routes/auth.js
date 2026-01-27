d9b8c3a6e8efe048c001dd4068d2b782e4841385
sujithavasireddy-24 <sujithavasireddy24@gmail.com>
2026-01-27 21:36:34 +0530

    Update backend routes for register and login

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -26,4 +26,29 @@ route.post("/create-user", async (req, res) => {
         return res.status(500).json({ "message": "internal server error while creating user" })
     }
 })
+
+route.post("/login",async (req,res)=>{
+    try{
+        const {email,password}=req.body 
+        const adminEmail="admin@gmail.com"
+        const adminPassword="Admin1@"
+        if(adminEmail==email && adminPassword==password){
+            return res.status(200).json({"message":"Admin login successful", role:"admin"})
+        }
+        const user=await User.findOne({email})
+        if(!user){
+            return res.status(400).json({"message":"Invalid email"})
+        }
+        const isMatch=await bcrypt.compare(password,user.password)
+
+        if(!isMatch){
+            return res.status(400).json({"message":"Invalid password"})
+        }
+        return res.status(200).json({"message":"Login succesful", role:"user",userId:user._id})
+    }
+    catch(err){
+        console.log("error from user login",err)
+    }
+})
+
 module.exports = route
